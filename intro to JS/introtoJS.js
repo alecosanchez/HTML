76da47da2adb3c3c64abea9087152492c3d2aa02
Aleco Sanchez <64794974+alecosanchez@users.noreply.github.com>
2022-03-30 11:03:43 -0400

    Create introtoJS.js

---
+++ intro to JS/introtoJS.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<script>
+        document.write("<h2>Thanks for visiting my page</h2>");
+        let sport=prompt("What is your favorite sport?");
+        document.write("<h2>"+sport+" is my favorite sport too!</h2>");
+        let day=prompt("What day is today?");
+        document.write("<h2>Today is <i>"+day+"</i>. Have a nice <i>"+day+"</i>!</h2>");
+    </script>
+</head>
+<body>
+</html>
